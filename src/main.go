fc256c0bfe22a843165613871ad516fa818c8f3a
yaojun111 <905699146@qq.com>
2019-08-02 18:54:42 +0800

    xx

--- src/main.go
+++ src/main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
+	"goServer/src/logger"
 	"goServer/src/room"
 	"net"
+	"os"
+	"os/signal"
 )
 
 func main() {
-
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	sig := <-c
+	logger.Debug_MSG("server close down(signal: %v)", sig)
 	listener, err := net.Listen("tcp", "localhost:6600")
 	if err != nil {
 		panic(err)
 	}
-	r  :=room.CreateRoom()
+	r := room.CreateRoom()
 	for {
 		conn, err := listener.Accept()
-		pl :=room.CreateConn(conn)
+		pl := room.CreateConn(conn)
 		r.AddConn(pl)
 		if err != nil {
 			panic(err)
@@ -24,4 +30,3 @@ func main() {
 		go pl.ReadMessage()
 	}
 }
-
--- src/room/Conn.go
+++ src/room/Conn.go
@@ -3,33 +3,72 @@ package room
 import (
 	"fmt"
 	"github.com/golang/protobuf/proto"
-	"net"
+	"goServer/src/logger"
 	pb "goServer/src/pb"
+	"net"
 	"sync"
 )
-var(
+
+var (
 	count uint64
 )
 
-type TcpConn struct{
+type TcpConn struct {
 	sync.Mutex
-	connid uint64
-	remoteAddr net.Addr
-	conn net.Conn
+	Addr            string
+	MaxConnNum      int
+	PendingWriteNum int
+	serverid        uint64
+	remoteAddr      net.Addr
+	conn            net.Conn
+	listener        net.Listener
 }
 
-
-func CreateConn(c net.Conn) *TcpConn{
-	tc :=&TcpConn{}
-	tc.connid =count
+func CreateConn(c net.Conn) *TcpConn {
+	tc := &TcpConn{}
+	tc.serverid = count
 	tc.conn = c
-	count +=1
+	count += 1
 	tc.remoteAddr = tc.conn.RemoteAddr()
 	return tc
 }
 
+func (c *TcpConn) InitTcpConn() {
+	var err error
+	c.listener, err = net.Listen("tcp", c.Addr)
+	if err != nil {
+		logger.Fatal_MSG("%v", err)
+	}
+	if c.MaxConnNum <= 0 {
+		c.MaxConnNum = 100
+	}
+	if c.PendingWriteNum <= 0 {
+		c.PendingWriteNum = 100
+	}
+}
+
+func (c *TcpConn) run() {
+	//var tempDelay time.Duration
+	r := CreateRoom()
+	for {
+		conn, err := c.listener.Accept()
+		if err != nil {
+			if ne, ok := err.(net.Error); ok && ne.Temporary() {
+
+			}
+		}
+		pl := CreateConn(conn)
+		r.AddConn(pl)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("new connect", conn.RemoteAddr())
+		go pl.ReadMessage()
+	}
+}
+
 //接收消息
-func (c *TcpConn)ReadMessage() {
+func (c *TcpConn) ReadMessage() {
 	buf := make([]byte, 409600)
 	for {
 		len := 0
@@ -55,11 +94,11 @@ func (c *TcpConn)ReadMessage() {
 	}
 }
 
-func (c *TcpConn)Close(){
+func (c *TcpConn) Close() {
 	c.conn.Close()
 }
 
-func (c *TcpConn)Destroy(){
+func (c *TcpConn) Destroy() {
 	c.Lock()
 	defer c.Unlock()
 }
